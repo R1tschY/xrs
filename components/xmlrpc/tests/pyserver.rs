1f1d3fbfcb557b03a20779a4aae9d5c3ecadf865
Richard Liebscher <r1tschy@posteo.de>
2022-12-22 21:42:44 +0100

    Improve integration test

--- components/xmlrpc/tests/pyserver.rs
+++ components/xmlrpc/tests/pyserver.rs
@@ -4,19 +4,12 @@ use xrs_xmlrpc::{MethodCall, MethodResponse};
 
 #[tokio::test]
 async fn test() -> Result<(), Box<dyn Error>> {
-    let client = reqwest::Client::new();
+    let client = xrs_xmlrpc::client::reqwest::XmlRpcClient::new("http://localhost:7777/RPC2");
 
-    let call = xrs_xmlrpc::ser::method_call_to_string("system.listMethods", &())?;
-    let res = client
-        .post("http://localhost:7777/RPC2")
-        .body(call)
-        .send()
-        .await?;
-
-    let text = res.text().await?;
-    let response: MethodResponse<Value> = xrs_xmlrpc::de::method_response_from_str(&text)?;
-
-    println!("{:?}", response);
+    let mut buf = String::new();
+    println!("{:?}", client.list_methods(&mut buf).await?);
+    let res: Value = client.call("xmlrpc.echo", &(1, 2, 3.0), &mut buf).await?;
+    println!("{:?}", res);
 
     Ok(())
 }
