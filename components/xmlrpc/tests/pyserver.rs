a9030072e95a4769be74363a85741bda5fff579a
Richard Liebscher <r1tschy@posteo.de>
2023-01-23 11:03:58 +0100

    Fix pyserver test

--- components/xmlrpc/tests/pyserver.rs
+++ components/xmlrpc/tests/pyserver.rs
@@ -1,10 +1,13 @@
 use std::error::Error;
+
 use xrs_xmlrpc::value::Value;
 use xrs_xmlrpc::{MethodCall, MethodResponse};
 
 #[tokio::test]
 async fn test() -> Result<(), Box<dyn Error>> {
-    let client = xrs_xmlrpc::client::reqwest::XmlRpcClient::new("http://localhost:7777/RPC2");
+    let client =
+        xrs_xmlrpc::client::reqwest::XmlRpcClientBuilder::new("http://localhost:7777/RPC2")
+            .build()?;
 
     let mut buf = String::new();
     println!("{:?}", client.list_methods(&mut buf).await?);
